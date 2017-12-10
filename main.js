0351bba7eb988edb382330a3626db214bab751f3
Mayur Joshi <endling1@gmail.com>
2017-12-10 23:00:50 +0530

    Add chapter 4 files

--- main.js
+++ main.js
@@ -5,15 +5,8 @@ const ipcMain = require('electron').ipcMain;
 
 let mainWindow = null;
 
-ipcMain.on('ping', (event, arg) => {
-	console.log('Main!');
-	if(arg === 'hello'){
-		event.sender.send('pong', 'Hello electron!');
-	}
-});
-
-app.on('ready', () => {
-	mainWindow = new BrowserWindow({
+function createMainWindow(){
+		mainWindow = new BrowserWindow({
 		width: 800, 
 		height: 600,
 		webPreferences: {
@@ -22,6 +15,63 @@ app.on('ready', () => {
 		} 
 	});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
+}
+
+function setupIpc(){
+	ipcMain.on('ping', (event, arg) => {
+		console.log('Main!');
+		if(arg === 'hello'){
+			event.sender.send('pong', 'Hello electron!');
+		}
+	});
+}
+
+function reportCrash(err){
+	console.log(err);
+}
+
+function crashApp(){
+	setTimeout(() => {
+		nonExistentFunction();
+	}, 2000);
+}
+
+app.on('will-finish-launching', () => {
+	console.log('will-finish-launching');
+});
+
+app.on('ready', () => {
+	console.log('ready');
+	createMainWindow();
+	setupIpc();
+	//crashApp();
+});
+
+app.on('window-all-closed', () => {
+	console.log('window-all-closed');
+	if(process.platform !== 'darwin'){
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	console.log('activate');
+	if(mainWindow === null){
+		createMainWindow();
+	}
+});
+
+app.on('before-quit', (evt) => {
+	console.log('before-quit');
+});
+
+app.on('quit', (evt, exitCode) => {
+	console.log('quit');
+});
+
+process.on('uncaughtException', (err) => {
+	reportCrash(err);
+	app.quit();
 });
 
 // window.ipcRenderer.on('pong', (event, arg) => {
