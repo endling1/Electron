8015dfaf15825277160a249c183ad56e102a0fc5
Mayur Joshi <endling1@gmail.com>
2017-12-10 22:29:11 +0530

    Add chapter 1 files

---
+++ main.js
@@ -0,0 +1,10 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({width: 800, height: 600});
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
