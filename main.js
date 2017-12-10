7ccd1609c95aa215891a86f71049977da6b24c6d
Mayur Joshi <endling1@gmail.com>
2017-12-10 22:40:51 +0530

    Add chapter two files

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipcMain = require('electron').ipcMain;
 
 let mainWindow = null;
 
+ipcMain.on('ping', (event, arg) => {
+	console.log('Main!');
+	if(arg === 'hello'){
+		event.sender.send('pong', 'Hello electron!');
+	}
+});
+
 app.on('ready', () => {
-	mainWindow = new BrowserWindow({width: 800, height: 600});
+	mainWindow = new BrowserWindow({
+		width: 800, 
+		height: 600,
+		webPreferences: {
+			nodeIntegration: false,
+			preload: __dirname + '/preLoad.js'
+		} 
+	});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
+
+// window.ipcRenderer.on('pong', (event, arg) => {
+// 		document.write('<h1>' + arg + '</h1>');
+// 	});
+// window.ipcRenderer.send('ping', 'hello');
